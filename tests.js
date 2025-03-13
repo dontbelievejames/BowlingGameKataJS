cd616a50eaadbb7e0e07c59ebc4afb1d8b2ce9ab
dontbelievejames <dontbelievejames@gmail.com>
2025-03-13 03:52:56 +0000

    x

--- tests.js
+++ tests.js
@@ -2,8 +2,8 @@ const { expect } = chai;
 
 describe('Array', function () {
     // To do - separate code and test
-    var game = { roll, function(pins) { },
-                score, function() { return 0}}
+    var game = { roll: function(pins) { },
+                score: function() { return 0}}
 
     for (let i = 0; i < 20; i++) {
         game.roll(0)
