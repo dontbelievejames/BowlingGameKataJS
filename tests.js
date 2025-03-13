c2361bfa2b82e762d56105b904b148cc99365759
dontbelievejames <dontbelievejames@gmail.com>
2025-03-13 03:34:51 +0000

    Adding mocha

--- tests.js
+++ tests.js
@@ -1,1 +1,7 @@
-console.log("Hi James")
+const { expect } = chai;
+
+describe('Array', function () {
+  it('should return -1 when the value is not present', function () {
+    expect([1, 2, 3].indexOf(4)).to.equal(-1);
+  });
+});
