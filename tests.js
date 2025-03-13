ad7c74735d748758d4c423a87959e94c071285a5
dontbelievejames <dontbelievejames@gmail.com>
2025-03-13 03:47:18 +0000

    adding first test

--- tests.js
+++ tests.js
@@ -1,7 +1,15 @@
-const { expect } = chai;
+//const { expect } = chai;
 
 describe('Array', function () {
+    // To do - separate code and test
+    var game = { roll, function(pins) { },
+                score, function() { return 0}}
+
+    for (let i = 0; i < 20; i++) {
+        game.roll(0)
+      }
+
   it('should return -1 when the value is not present', function () {
-    expect([1, 2, 3].indexOf(4)).to.equal(-1);
+    expect(game.score()).to.equal(1);
   });
 });
